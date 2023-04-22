921721a06ca198d9ff10744510e23f4c22c8619c
Zakharias Priya <priyazakharias@gmail.com>
2023-04-22 12:54:33 +0700

    final

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -98,7 +98,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
               <>
                 <MenuItem
                   onClick={() => router.push("/")}
-                  label={`Hello ${currentUser.name}`}
+                  label={`Hello, ${currentUser.name}`}
                 />
                 <MenuItem
                   onClick={() => router.push("/trips")}
